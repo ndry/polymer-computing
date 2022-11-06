d5ff9938928c349095d43a4fed45d86d4b22bbe9
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2022-11-06 10:23:50 +0200

    separated MainPanel

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import "./initAnalytics";
 
 import { css, cx } from "@emotion/css";
 import { useState } from "react";
-import { appVersion } from "./appVersion";
 import { stepInPlace } from "./puzzle/stepInPlace";
 import { initialWorld } from "./puzzle/terms";
 import { fishSolution as solution } from "./hardcodedSolutions";
 
-import { PlaybackPanel } from "./PlaybackPanel";
-import { SourceLineEditor } from "./SourceLineEditor";
 import { MainScene } from "./MainScene";
 import { Canvas } from "@react-three/fiber";
 import { DefaultOrbitControls } from "./utils/DefaultOrbitControls";
+import { MainPanel } from "./MainPanel";
 
 export function App() {
     // const world = useRecoilValue(worldRecoil);
@@ -52,82 +50,7 @@ export function App() {
             <DefaultOrbitControls />
             <MainScene world={world} />
         </Canvas>
-        <div className={cx(css({
-            position: "absolute",
-            bottom: 0,
-            left: 0,
-            right: 0,
-            pointerEvents: "none",
-        }))}>
-            <div className={cx(css({
-                position: "relative",
-            }))}>
-                <PlaybackPanel
-                    stepState={stepState}
-                    className={cx(css({
-                        position: "absolute",
-                        margin: "0 0 0 25px",
-                        background: "salmon",
-                        borderRadius: "8px 8px 0px 0px",
-                        padding: "5px 10px 0px 10px",
-                        bottom: 0,
-                        pointerEvents: "all",
-                    }))} />
-                <div className={cx(css({
-                    position: "absolute",
-                    padding: "5px 20px 3px 10px",
-                    textAlign: "right",
-                    lineHeight: "0.8rem",
-                    fontSize: "24px",
-                    color: "salmon",
-                    opacity: 0.5,
-                    bottom: 0,
-                    right: 0,
-                }))}>
-                    {appVersion.split("+")[0]}<br />
-                    <span className={css({ fontSize: "0.5em", })} >{appVersion.split("+")[1]}</span>
-                </div>
-            </div>
-            <div className={cx(css({
-                height: 200,
-                background: "salmon",
-                borderRadius: "15px 15px 0px 0px",
-                margin: "0px 10px 0px 10px",
-                pointerEvents: "all",
-                overflow: "scroll",
-            }))}>
-                <div
-                    className={cx(css({
-                        padding: "5px",
-                        width: "fit-content",
-                    }))}
-                >
-                    {
-                        solution.sources.map((source, i) => {
-                            return <div
-                                className={cx(css({
-                                    margin: "5px",
-                                    background: "grey",
-                                    whiteSpace: "nowrap",
-                                }))}
-                            >
-                                {source.mainLoop.map((line, j) => <SourceLineEditor
-                                    className={cx(
-                                        css({
-                                            width: 30,
-                                            display: "inline-block",
-                                            margin: "3px",
-                                            background: (j === step % solution.sources[i].mainLoop.length) ? "yellow" : "lightgrey",
-                                        })
-                                    )}
-                                    line={line}
-                                />)}
-                            </div>
-                        })
-                    }
-                </div>
-            </div>
-        </div>
+        <MainPanel stepState={stepState} />
 
     </div>
 }
