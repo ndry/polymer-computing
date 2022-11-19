3d5efe5629d5cc05205b32178b09e4151d54a745
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2022-11-19 12:39:17 +0200

    named solutions in sidepanel

--- src/SidePanel.tsx
+++ src/SidePanel.tsx
@@ -1,13 +1,12 @@
 import { css, cx } from "@emotion/css";
 import { useRecoilState } from "recoil";
 import { solutionRecoil } from "./solutionRecoil";
-import { fishSolution, newTetrahedron, shortTestSolution, tetrahedron } from "./hardcodedSolutions";
+import * as solutions from "./hardcodedSolutions";
 
 export function SidePanel({
     className, ...props
 }: {} & JSX.IntrinsicElements["div"]) {
     const [solution, setSolution] = useRecoilState(solutionRecoil);
-    const solutions = [shortTestSolution, tetrahedron, newTetrahedron, fishSolution];
 
     return <div
         className={cx(
@@ -21,10 +20,13 @@ export function SidePanel({
         {...props}
     >
         {
-            solutions.map((x, counter) => <li className={cx(css({
-                background: x == solution ? "#108081a0" : ""
-            }))}
-                onClick={() => setSolution(x)}>{"solution: " + ++counter}</li>)
+            Object.entries(solutions).map(([key, s]) => <li
+                key={key}
+                className={cx(css({
+                    background: s === solution ? "#108081a0" : ""
+                }))}
+                onClick={() => setSolution(s)}
+            >{key}</li>)
         }
     </div>
 }
